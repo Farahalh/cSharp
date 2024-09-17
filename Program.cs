0aba107da80da4d449eda1d60c016cd005389501
FarahAlh <farah.alhaddad@hotmail.com>
2024-09-17 15:29:57 +0200

    Selection Statements

--- Program.cs
+++ Program.cs
@@ -2,18 +2,51 @@
 
 class Program
 {
-    //    Create a C# program that requests three names of people from the user -check
-    //
-    //    and stores them in an array of objects of type Person. - check
-    //
-    //    To do this, first create a Person class that has a Name property of type string - check
-    //
-    //    and override the ToString() method. - check
-    //
-    //End the program by reading persons and executing the ToString() method on the screen.
-
     static void Main(string[] args)
+    {
+        // Övning - Selektion
+        // Gör ett program som använder sig av nedanstående:
+
+        // - if statements
+        int x = 20;
+        int y = 18;
+        if (x > y)
         {
-            
+            Console.WriteLine("x is greater than y");
         }
+
+        // - if else statements
+        int time = 22;
+        if (time < 10)
+        {
+            Console.WriteLine("Good morning.");
+        }
+        else if (time < 20)
+        {
+            Console.WriteLine("Good day.");
+        }
+        else
+        {
+            Console.WriteLine("Good evening.");
+        }
+
+        // - switch statements
+        // switch (expression)
+        // {
+        //     case x:
+        //         // code block
+        //         break;
+        //     case y:
+        //         // code block
+        //         break;
+        //     default:
+        //         // code block
+        //         break;
+        // }
+
+        // - ternary operator
+        //variable = (condition) ? expressionTrue : expressionFalse;
+        int age = 20;
+        string greeting = age >= 18 ? "Welcome!" : "No minors allowed!";
+    }
 }
