cb70055b88ac2cee2a7295b6bd45de224454d9ca
FarahAlh <farah.alhaddad@hotmail.com>
2024-09-17 15:03:29 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace cSharp;
+
+class Program
+{
+    //    Create a C# program that requests three names of people from the user -check
+    //
+    //    and stores them in an array of objects of type Person. - check
+    //
+    //    To do this, first create a Person class that has a Name property of type string - check
+    //
+    //    and override the ToString() method. - check
+    //
+    //End the program by reading persons and executing the ToString() method on the screen.
+
+    static void Main(string[] args)
+        {
+            
+        }
+}
