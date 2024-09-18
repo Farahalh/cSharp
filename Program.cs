5c691ed806d237b3281ccd7a0daeba94562b4156
FarahAlh <farah.alhaddad@hotmail.com>
2024-09-18 16:05:33 +0200

    Interactive Program Menu

--- Program.cs
+++ Program.cs
@@ -7,28 +7,28 @@ class Program
         // Övning - Selektion
         // Gör ett program som använder sig av nedanstående:
 
-        // - if statements
-        int x = 20;
-        int y = 18;
-        if (x > y)
-        {
-            Console.WriteLine("x is greater than y");
-        }
+        // // - if statements
+        // int x = 20;
+        // int y = 18;
+        // if (x > y)
+        // {
+        //     Console.WriteLine("x is greater than y");
+        // }
 
-        // - if else statements
-        int time = 22;
-        if (time < 10)
-        {
-            Console.WriteLine("Good morning.");
-        }
-        else if (time < 20)
-        {
-            Console.WriteLine("Good day.");
-        }
-        else
-        {
-            Console.WriteLine("Good evening.");
-        }
+        // // - if else statements
+        // int time = 22;
+        // if (time < 10)
+        // {
+        //     Console.WriteLine("Good morning.");
+        // }
+        // else if (time < 20)
+        // {
+        //     Console.WriteLine("Good day.");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Good evening.");
+        // }
 
         // - switch statements
         // switch (expression)
@@ -44,9 +44,37 @@ class Program
         //         break;
         // }
 
-        // - ternary operator
-        //variable = (condition) ? expressionTrue : expressionFalse;
-        int age = 20;
-        string greeting = age >= 18 ? "Welcome!" : "No minors allowed!";
+        // // - ternary operator
+        // //variable = (condition) ? expressionTrue : expressionFalse;
+        // int age = 20;
+        // string greeting = age >= 18 ? "Welcome!" : "No minors allowed!";
+
+
+        // Övning - Program meny
+        Console.WriteLine("Choose an option from the menu:");
+        Console.WriteLine("1. Say Hello");
+        Console.WriteLine("2. Display today's date");
+        Console.WriteLine("3. Exit");
+
+        string input = Console.ReadLine();
+
+        int choice = int.Parse(input);
+
+        if (choice == 1)
+        {
+            Console.WriteLine("Hello!");
+        }
+        else if (choice == 2)
+        {
+            Console.WriteLine($"Today's date is: {DateTime.Now.ToShortDateString()}");
+        }
+        else if (choice == 3)
+        {
+            Console.WriteLine("Goodbye!");
+        }
+        else
+        {
+            Console.WriteLine("Invalid choice, please try again.");
+        }
     }
 }
