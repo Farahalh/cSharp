79fe70d8d855b30bb8ebfb6b3d5a0f3bf12739a3
Farah Alhaddad <farah.alhaddad@hotmail.com>
2024-09-23 13:59:28 +0200

    Try out nullable types, null-conditional operator, null-coalescing operator and try-catch

--- Program.cs
+++ Program.cs
@@ -2,6 +2,36 @@
 
 class Program
 {
+    static void PrintMessage(string s)
+    {
+        Console.WriteLine(s.Length);
+    }
+
+    static void DivisionOfTwoNumbers()
+    {
+        int dividend = 10;
+        int divisor = 0;
+
+        Console.WriteLine(dividend / divisor);
+    }
+
+    static void DivisionOfUserInput()
+    {
+        int divisorInput;
+        int dividendInput;
+
+        Console.WriteLine("Input value one: ");
+        divisorInput = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Input value two: ");
+        dividendInput = Convert.ToInt32(Console.ReadLine());
+
+        int result = dividendInput / divisorInput;
+
+        Console.WriteLine("Result: ");
+        Console.WriteLine($"{dividendInput} / {divisorInput} = {result}");
+    }
+
     static void Main(string[] args)
     {
         // Övning - Selektion
@@ -49,21 +79,42 @@ class Program
         // int age = 20;
         // string greeting = age >= 18 ? "Welcome!" : "No minors allowed!";
 
-        var displaysMenu = new Menu();
-        displaysMenu.DisplaysMenu();
+        //var displaysMenu = new Menu();
+        //displaysMenu.DisplaysMenu();
 
 
-        // // Om man ska skapa upp ett objekt(instans) så behöver man ta bort static på metoden så att ctor kan komma åt datan på objektet(instansen)
-        var personsDisplayer = new PersonsDisplayer(new ConsoleIO());
-        personsDisplayer.DisplayPersons();
+        //// // Om man ska skapa upp ett objekt(instans) så behöver man ta bort static på metoden så att ctor kan komma åt datan på objektet(instansen)
+        //var personsDisplayer = new PersonsDisplayer(new ConsoleIO());
+        //personsDisplayer.DisplayPersons();
 
-        // // Här kommer man åt datan genom static på metoden
-        // PersonsDisplayer.DisplayPersons();
+        //// // Här kommer man åt datan genom static på metoden
+        //// PersonsDisplayer.DisplayPersons();
 
-        var loopsDisplayer = new Loops();
-        loopsDisplayer.methodWhile();
-        loopsDisplayer.methodDo();
-        loopsDisplayer.methodFor();
+        //var loopsDisplayer = new Loops();
+        //loopsDisplayer.methodWhile();
+        //loopsDisplayer.methodDo();
+        //loopsDisplayer.methodFor();
 
+
+        string? s = (args.Length > 0) ? args[0] : null;
+        PrintMessage(s ?? string.Empty);
+
+        try
+        {
+            DivisionOfTwoNumbers();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.StackTrace);
+        }
+
+        try
+        {
+            DivisionOfUserInput();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
