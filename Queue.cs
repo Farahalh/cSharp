d5bd57e0c47bf08a2cc353e7a657674cb30ffdf3
Farah Alhaddad <farah.alhaddad@hotmail.com>
2024-09-26 13:47:28 +0200

    Create Queue, Enqueue & print, Dequeue until Queue is empty & print, Enqueue.

---
+++ Queue.cs
@@ -0,0 +1,52 @@
+﻿namespace cSharp;
+
+class Queue
+{
+
+    // Create a Queue that stores strings
+    // Add three strings in queue
+    // Write a method that removes the first string in the queue and writes it to the console
+    // Add two more strings and repeate remove until the queue is empty
+    // Use the methods Enqueu and Dequeue
+
+    Queue<string> queue = new Queue<string>();
+
+    public Queue()
+    {
+
+    }
+
+    public void modifyQueue()
+    {
+        Console.WriteLine("Original Queue: ");
+
+        queue.Enqueue("Spoon");
+        queue.Enqueue("Fork");
+        queue.Enqueue("Knife");
+
+        foreach (var item in queue)
+        {
+            Console.WriteLine(item);
+        }
+
+        //queue.Dequeue();
+
+        Console.WriteLine("\nDequeued Queue: ");
+
+        while (queue.Count > 0)
+        {
+            var dequeueItem = queue.Dequeue();
+            Console.WriteLine(dequeueItem);
+        }
+
+        Console.WriteLine("\nUpdated Queue: ");
+
+        queue.Enqueue("Plate");
+        queue.Enqueue("Cup");
+
+        foreach (var item in queue)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
